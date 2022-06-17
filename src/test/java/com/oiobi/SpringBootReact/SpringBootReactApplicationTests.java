7ed3580a0e45370c2e73e312aa149b4bea56b0b2
thanhbuynitu <thanhbuynitu@gmail.com>
2022-06-17 22:38:05 +0700

    Commit build success init

---
+++ src/test/java/com/oiobi/SpringBootReact/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oiobi.SpringBootReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
